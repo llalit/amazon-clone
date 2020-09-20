165801c0e5fbdf57b730be74cee9e5e0929fcd9b
llalit <erlalit.lahoria@gmail.com>
2020-09-20 21:02:59 +0530

    Updates

---
+++ precache-manifest.29a871922097913b26e8e13492e6d614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685c465371c9ba3c1cf5e006bbcff203",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "9b4990e03d719fc6c111",
+    "url": "/amazon-clone/static/css/main.e3b04097.chunk.css"
+  },
+  {
+    "revision": "1bd3a4ad3acead1987f7",
+    "url": "/amazon-clone/static/js/2.f265e094.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon-clone/static/js/2.f265e094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4990e03d719fc6c111",
+    "url": "/amazon-clone/static/js/main.10752846.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  },
+  {
+    "revision": "80775cf681f17a79015d6f671b975cf0",
+    "url": "/amazon-clone/static/media/amazon__logo.80775cf6.png"
+  }
+]);
